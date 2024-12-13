524227f6c5a5fa9ccf93125fdf9422c4fbdd201b
MaxMight01 <ramdassingh399@gmail.com>
2024-12-13 15:14:49 +0530

    Added movement in game.py.

--- src/game.py
+++ src/game.py
@@ -16,5 +16,15 @@ def set_screen_res(width, height):
     view.volume.update_viewport()
 
 #Enter pygame.key.get_pressed() here. Goes outside the `event in pygame.event.get()`.
-def movement(keys):
-    continue
+def movement(keys, step):
+    keys_pressed = keys[pygame.K_w] + keys[pygame.K_a] + keys[pygame.K_s] + keys[pygame.K_d]
+    W = keys[pygame.K_w]*np.array([[0],[0],[-step]])
+    A = keys[pygame.K_a]*np.array([[-step],[0],[0]])
+    S = keys[pygame.K_s]*np.array([[0],[0],[step]])
+    D = keys[pygame.K_d]*np.array([[step],[0],[0]])
+
+    if keys_pressed == 1 or keys_pressed == 3:
+        camera.update_position_movement(W+A+S+D)
+    elif keys_pressed == 2:
+        camera.update_position_movement(0.707*(W+A+S+D))
+    view_volume.get_camera(camera)
